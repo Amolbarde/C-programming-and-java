640bc400b0adef851be0c0e08a0973d574485ece
Amol Barde <138032586+Amolbarde@users.noreply.github.com>
2025-09-02 21:10:50 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int i,num;
+  printf("Enter number:");
+  scanf("%d",&num);
+  if(num<=1)
+  {
+    printf("%d is not a prime number",num);
+    return 0;
+  }
+for(i=2;i<num;i++)
+{
+ if(num%i==0)
+ {
+  printf("%d is not a prime number",num);
+ return 0;
+ }
+}
+printf("%d is  a prime number",num);
+}
